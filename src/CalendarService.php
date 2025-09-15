6995f5bafa498a423fe53c51d7858018ccd23fb9
Martin Giessing <mgp@novicell.dk>
2025-09-15 12:39:10 +0200

    Include configs for layout builder on events

--- src/CalendarService.php
+++ src/CalendarService.php
@@ -2,13 +2,30 @@
 namespace Drupal\premium_calendar;
 
 use Drupal\block_content\Entity\BlockContent;
+use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Field\EntityReferenceFieldItemList;
+use Drupal\Core\Language\LanguageManagerInterface;
 
 class CalendarService {
 
+  /**
+   * @var \Drupal\Core\Language\LanguageManagerInterface
+   */
+  private LanguageManagerInterface $languageManager;
+
+  /**
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  private ModuleHandlerInterface $moduleHandler;
+
+  function __construct(LanguageManagerInterface $languageManager, ModuleHandlerInterface $moduleHandler) {
+    $this->languageManager = $languageManager;
+    $this->moduleHandler = $moduleHandler;
+  }
+
   function getCalendarOptions(BlockContent $block) {
     $settings = ['options' => []];
-    $settings['options']['locale'] = \Drupal::languageManager()->getCurrentLanguage()->getId();
+    $settings['options']['locale'] = $this->languageManager->getCurrentLanguage()->getId();
     $settings['options']['timeZone'] = date_default_timezone_get();
     $settings['options']['initialView'] = str_replace('calendar--', '', $block->get('field_display')->first()->getString());
     $settings['options']['eventSources'] = [];
@@ -24,6 +41,7 @@ class CalendarService {
         ];
       }
     }
+    $this->moduleHandler->alter('calendar_options', $settings);
 
     return $settings;
   }
